5ed87ace65eb5c7478ca9afc00c475cfad8d419b
Parker Ackerman <plackerm@mtu.edu>
2019-02-22 23:07:16 -0500

    Added on_ready event and roll command. Rolls up to 20 dice at once.

--- SheetBot.py
+++ SheetBot.py
@@ -2,4 +2,41 @@ import discord
 from discord.ext import commands
 import random
 
+TOKEN = "NTQ4NzA2MTEyNzM2NzIyOTQ0.D1JQvA.Y_7u1wv6Wt-iSA9J7G17WvS1JSU"  # Stores bot token. KEEP THIS PRIVATE
+client = commands.Bot(command_prefix=",")   # Create new client object
 
+presence = "God"    # Rich presence phrase. 'Playing' will be put before this automatically.
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=discord.Game(name=presence))
+    print("SheetBot: Ready to roll")
+
+
+@client.command()
+async def open_sheet(name="opensheet"):
+
+
+
+@client.command()
+async def roll(str):
+    if "d" in str:
+        index = str.find("d")
+        num = int(str[0:index])
+        if num > 20:
+            await client.say("Invalid input: Too many dice.")
+            return
+        sides = int(str[index+1:len(str)])
+        lis = []
+        outstr = ""
+        for n in range(num):
+            lis.append(random.randrange(1, sides+1))
+
+        for num in lis:
+            outstr += f"You rolled: **{num}**!\n"
+
+        await client.say(outstr)
+    else:
+        await client.say("Invalid input! Please try again.")
+
+client.run( TOKEN )
