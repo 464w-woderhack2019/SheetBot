aa81da122ce1323a5f0ad7d0fbb9e4f9e8112ea0
Parker Ackerman <plackerm@mtu.edu>
2019-02-23 18:26:00 -0500

    Removed some unneeded lines. Comments added.

--- SheetBot.py
+++ SheetBot.py
@@ -68,18 +68,25 @@ SPELLS
 GEAR
 --
 '''
-
+# Token the bot uses
 TOKEN = "NTQ4NzA2MTEyNzM2NzIyOTQ0.D1JQvA.Y_7u1wv6Wt-iSA9J7G17WvS1JSU"  # Stores bot token. KEEP THIS PRIVATE
+
+# Creating new bot object
 client = commands.Bot(command_prefix=".")   # Create new client object
 
 presence = "God"    # Rich presence phrase. 'Playing' will be put before this automatically.
 
+# List to store currently opened sheets
 sheets = []
 
 
+#   Stores all information for a 5e character sheet
 class Sheet:
 
+    #   Character name
     char_name = ""
+
+    #   Dicts for information
     basics_dict = {
         "player": 0,
         "name": 1,
@@ -134,16 +141,11 @@ class Sheet:
         "cha": 5,
         "charisma": 5
     }
+
+    #   Arrays for storing values on a sheet-by-sheet basis
     basics = []
     info = []
-    stats = [
-        None,
-        None,
-        None,
-        None,
-        None,
-        None
-    ]
+    stats = []
     saves = []
     nums = []
     langs = []  #Array of strings (languages)
@@ -151,14 +153,16 @@ class Sheet:
     spells = [] #Array of arrays. Inner array: [Spell name, spell level, range, VSM, effect]
     gear = []   #Array of arrays. Inner array: [Gear name, effects]
 
+    # Constructor. Initializes values. In the future, should check if file exists and
+    # load vals if it does.
     def __init__(self, name):
         self.stats = [
-            self.stats[0],  #STR
-            self.stats[1],  #DEX
-            self.stats[2],  #CON
-            self.stats[3],  #INT
-            self.stats[4],  #WIS
-            self.stats[5]   #CHA
+            None,  #STR
+            None,  #DEX
+            None,  #CON
+            None,  #INT
+            None,  #WIS
+            None   #CHA
         ]
         self.char_name = name
         self.saves = [
@@ -209,19 +213,19 @@ class Sheet:
         print()#   This problem is left as an exercise to the reader
 
 
+# Does something when bot goes online
 @client.event
 async def on_ready():
+    # Changes rich presence to presence variable
     await client.change_presence(game=discord.Game(name=presence))
+    # Prints ready message to console
     print("SheetBot: Ready to role(play)")
 
 
-@client.event
-async def on_error():
-    print("ERROR: Unknown")
-
-
+# Says a list of open character sheets
 @client.command()
 async def get_open(name="getopen"):
+    # Checks if any sheets are open currently
     if len(sheets) > 0:
         out_str = ""
         for sh in sheets:
@@ -229,6 +233,7 @@ async def get_open(name="getopen"):
         await client.say(out_str)
     else:
         await client.say("No character sheets open at this time.")
+
 
 @client.command()
 async def open_char(char_name):
@@ -250,7 +255,6 @@ async def get_stats(file_name):
             found = True
             sh = s
     if not found:
-        found = False
         await client.say( "Sheet not found. Open a sheet with the char name." )
         return
     out_str = ""
@@ -261,6 +265,7 @@ async def get_stats(file_name):
     await client.say(out_str)
 
 
+# Set all stats for a character
 @client.command()
 async def set_stats(file_name, stre, dex, con, inte, wis, cha):
     sh = None
@@ -272,9 +277,9 @@ async def set_stats(file_name, stre, dex, con, inte, wis, cha):
             found = True
             sh = s
     if not found:
-        found = False
         await client.say( "Sheet not found. Open a sheet with the char name." )
         return
+
     place = 0;
     try:
         sh.stats[0] = int(stre)
@@ -292,6 +297,7 @@ async def set_stats(file_name, stre, dex, con, inte, wis, cha):
         await client.say(f"Invalid score inputted at index {str(place)}")
 
 
+# Sets an individual stat for a sheet
 @client.command()
 async def set_stat(file_name, a_score, val, name="setstat"):
     sh = None
@@ -303,24 +309,24 @@ async def set_stat(file_name, a_score, val, name="setstat"):
             found = True
             sh = s
     if not found:
-        found = False
         await client.say("Sheet not found. Open a sheet with the char name.")
         return
-    print("AAAHHHH")
+
     a_index = sh.stat_dict.get(a_score.lower(), -1)
     print("Index: " + str(a_index))
 
     if a_index < 0 or a_index > 5:
-        print("Input invalid")
+        await client.say("Input invalid")
     else:
         try:
             print(sh.char_name)
             sh.stats[a_index] = int(val)
             print(sh.stats[a_index])
         except:
-            print("Invalid score inputted")
+            await client.say("Invalid score inputted")
 
 
+# Get a single statistic from a sheet
 @client.command()
 async def get_stat(file_name, a_score):
     sh = None
@@ -338,20 +344,19 @@ async def get_stat(file_name, a_score):
         return
 
     #sh = auth(file_name)   Not doing this anymore
-    if sh == -1:
-        return
 
     a_index = sh.stat_dict.get(a_score, -1)
 
     if a_index < 0 or a_index > 5:
-        print("Input invalid")
+        await client.say("Input invalid")
     else:
         try:
             await client.say(str(sh.stats[a_index]))
         except:
-            print("Print failed")
+            await client.say("Print failed")
 
 
+#   Prints a character sheet to the discord channel
 @client.command()
 async def print_sheet(file_name, name="printsheet"):
     sheet = open(file_name, "r")
@@ -375,6 +380,7 @@ async def print_sheet(file_name, name="printsheet"):
 # Calling functions with coroutines is hard
 
 
+# Rolls x y-sided dice
 @client.command()
 async def roll(str):
     if "d" in str:
@@ -396,4 +402,5 @@ async def roll(str):
     else:
         await client.say("Invalid input! Please try again.")
 
+# Runs the client using the token
 client.run( TOKEN )
